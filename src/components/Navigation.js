e8794cb80cbf41d3a3577c35a71fe88094c05206
Carsten Lund <clu@rts.dk>
2023-03-15 10:30:27 +0100

    add pre commit hook and fix navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,8 +4,8 @@ const Navigation = () => {
             <menu>
                 <li><a href="#">Contact</a></li>
                 <li><a href="#">About</a></li>
+                <li><a href="#">Terms & Conditions</a></li>
             </menu>
-            <li><a href="#">Terms & Conditions</a></li>
         </nav>
      );
 }
